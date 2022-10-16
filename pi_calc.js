0ea3e7fd9f569ba1ad97f42c30269292225fbea9
khanhf-ng820 <115331423+khanhf-ng820@users.noreply.github.com>
2022-10-16 11:09:18 +0700

    Add files via upload

---
+++ pi_calc.js
@@ -0,0 +1,27 @@
+document.getElementById('result').style.height = document.getElementsByClassName('item2')[0].getBoundingClientRect()['height'];
+document.getElementById('result').style.width = document.getElementsByClassName('item4')[0].getBoundingClientRect()['width'];
+
+
+
+window.addEventListener('resize', function(event) {
+	document.getElementById('result').style.height = document.getElementsByClassName('item2')[0].getBoundingClientRect()['height'];
+	document.getElementById('result').style.width = document.getElementsByClassName('item4')[0].getBoundingClientRect()['width'];
+}, true);
+
+
+function piproduct() {
+	let e = nerdamer(document.getElementById('expr').value);
+	let lower = parseInt(document.getElementById('lower').value || 0);
+	let upper = parseInt(document.getElementById('upper').value || 0);
+	let product = 1;
+
+	try {
+		let f = e.buildFunction(['n']);
+		for (let i = lower; i <= upper; i++) {
+			product *= f(i);
+		}
+		document.getElementById('result').innerHTML = product;
+	} catch {
+		document.getElementById('result').innerHTML = undefined;
+	}
+}
